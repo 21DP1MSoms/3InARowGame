2886249ad6295c6604c526ac7f073ab75f9ef546
Mārcis Soms-Sakne <106299130+21DP1MSoms@users.noreply.github.com>
2022-11-07 10:50:49 +0200

    update
    made the code more readable (spaces)

--- 3InARow/main.cpp
+++ 3InARow/main.cpp
@@ -3,20 +3,20 @@
 #include <iostream>
 
 int ts = 65; //tile size
-sf::Vector2i offset(55, 31); // 48 24
+sf::Vector2i offset(55, 31);
 
 struct piece
-{ int x,y,col,row,kind,match,alpha;
-    piece(){match=0; alpha=255;}
+{ int x, y, col, row, kind, match, alpha;
+    piece(){match = 0; alpha = 255;}
 } grid[10][10];
 
-void swap(piece p1,piece p2)
+void swap(piece p1, piece p2)
 {
-    std::swap(p1.col,p2.col);
-    std::swap(p1.row,p2.row);
+    std::swap(p1.col, p2.col);
+    std::swap(p1.row, p2.row);
 
-    grid[p1.row][p1.col]=p1;
-    grid[p2.row][p2.col]=p2;
+    grid[p1.row][p1.col] = p1;
+    grid[p2.row][p2.col] = p2;
 }
 
 
@@ -38,15 +38,15 @@ int main()
     for (int i = 1; i <= 9; i++)
         for (int j = 1; j <= 9; j++)
         {
-            grid[i][j].kind = rand()%3;
+            grid[i][j].kind = rand() %3;
             grid[i][j].col  = j;
             grid[i][j].row  = i;
-            grid[i][j].x    = j*ts;
-            grid[i][j].y    = i*ts;
+            grid[i][j].x    = j * ts;
+            grid[i][j].y    = i * ts;
         }
 
-    int x0,y0,x,y; int click=0; sf::Vector2i pos;
-    bool isSwap=false, isMoving=false;
+    int x0, y0, x, y; int click = 0; sf::Vector2i pos;
+    bool isSwap = false, isMoving = false;
 
     while (game.isOpen())
     {
@@ -65,62 +65,62 @@ int main()
         }
 
         /// mouse click
-        if (click==1)
+        if (click == 1)
         {
-            x0=pos.x/ts+1;
-            y0=pos.y/ts+1;
+            x0 = pos.x / ts + 1;
+            y0 = pos.y / ts + 1;
         }
-        if (click==2)
+        if (click == 2)
         {
-            x=pos.x/ts+1;
-            y=pos.y/ts+1;
-            if (abs(x-x0)+abs(y-y0)==1)
-            {swap(grid[y0][x0],grid[y][x]); isSwap=1; click=0;}
-            else click=1;
+            x = pos.x / ts + 1;
+            y = pos.y / ts + 1;
+            if (abs(x - x0) + abs(y - y0) == 1)
+            {swap(grid[y0][x0], grid[y][x]); isSwap = 1; click = 0;}
+            else click = 1;
         }
 
         /// Match finding
         int points = 0;
-        for(int i=1;i<=9;i++)
-            for(int j=1;j<=9;j++)
+        for(int i = 1; i <= 9; i++)
+            for(int j = 1; j <= 9; j++)
             {
-                if (grid[i][j].kind==grid[i+1][j].kind)
-                    if (grid[i][j].kind==grid[i-1][j].kind)
-                        for(int n=-1;n<=1;n++)
-                            grid[i+n][j].match++;
+                if (grid[i][j].kind == grid[i + 1][j].kind)
+                    if (grid[i][j].kind == grid[i - 1][j].kind)
+                        for(int n =- 1 ; n <= 1; n++)
+                            grid[i + n][j].match++;
 
-                if (grid[i][j].kind==grid[i][j+1].kind)
-                    if (grid[i][j].kind==grid[i][j-1].kind)
-                        for(int n=-1;n<=1;n++)
-                            grid[i][j+n].match++;
+                if (grid[i][j].kind == grid[i][j + 1].kind)
+                    if (grid[i][j].kind == grid[i][j - 1].kind)
+                        for(int n =- 1; n <= 1; n++)
+                            grid[i][j + n].match++;
             }
 
 
         /// Moving animation
-        isMoving=false;
-        for (int i=1;i<=9;i++)
-            for (int j=1;j<=9;j++)
+        isMoving = false;
+        for (int i = 1; i <= 9; i++)
+            for (int j = 1; j <= 9; j++)
             {
                 piece &p = grid[i][j];
-                int dx,dy;
-                for(int n=0;n<4;n++)   // 4 - speed
-                {dx = p.x-p.col*ts;
-                    dy = p.y-p.row*ts;
-                    if (dx) p.x-=dx/abs(dx);
-                    if (dy) p.y-=dy/abs(dy);}
-                if (dx||dy) isMoving=1;
+                int dx, dy;
+                for(int n = 0; n < 4; n++)   // 4 - speed
+                {dx = p.x - p.col * ts;
+                    dy = p.y - p.row * ts;
+                    if (dx) p.x -= dx / abs(dx);
+                    if (dy) p.y -= dy / abs(dy);}
+                if (dx||dy) isMoving = 1;
             }
 
         /// Deleting animation
         if (!isMoving)
-            for (int i=1;i<=9;i++)
-                for (int j=1;j<=9;j++)
+            for (int i = 1; i <= 9; i++)
+                for (int j = 1; j <= 9; j++)
                     if (grid[i][j].match) if (grid[i][j].alpha>10) {grid[i][j].alpha-=10; isMoving=true;}
 
         /// Get score
-        int score=0;
-        for (int i=1;i<=9;i++)
-            for (int j=1;j<=9;j++)
+        int score = 0;
+        for (int i = 1; i <= 9; i++)
+            for (int j = 1; j <= 9; j++)
                 score+=grid[i][j].match;
 
 
@@ -132,19 +132,19 @@ int main()
         /// Update grid
         if (!isMoving)
         {
-            for(int i=9;i>0;i--)
-                for(int j=1;j<=9;j++)
+            for(int i = 9; i > 0; i--)
+                for(int j = 1; j <= 9; j++)
                     if (grid[i][j].match)
-                        for(int n=i;n>0;n--)
-                            if (!grid[n][j].match) {swap(grid[n][j],grid[i][j]); break;};
+                        for(int n = i; n > 0; n--)
+                            if (!grid[n][j].match) {swap(grid[n][j], grid[i][j]); break;};
 
-            for(int j=1;j<=9;j++)
-                for(int i=9,n=0;i>0;i--)
+            for(int j = 1; j <= 9; j++)
+                for(int i=9, n = 0; i > 0; i--)
                     if (grid[i][j].match)
                     {
-                        grid[i][j].kind = rand()%6;
-                        grid[i][j].y = -ts*n++;
-                        grid[i][j].match=0;
+                        grid[i][j].kind  = rand() %6;
+                        grid[i][j].y     = -ts * n++;
+                        grid[i][j].match = 0;
                         grid[i][j].alpha = 255;
                     }
         }
@@ -157,14 +157,14 @@ int main()
         board.setPosition(48, 24);
         board.setScale(3.4f, 3.4f);
 
-        for (int i=1;i<=9;i++)
-            for (int j=1;j<=9;j++)
+        for (int i = 1; i <= 9; i++)
+            for (int j = 1; j <= 9; j++)
             {
                 piece p = grid[i][j];
-                gems.setTextureRect( sf::IntRect(p.kind*64,0,64,64));
-                gems.setColor(sf::Color(255,255,255,p.alpha));
-                gems.setPosition(p.x,p.y);
-                gems.move(offset.x-ts,offset.y-ts);
+                gems.setTextureRect( sf::IntRect(p.kind*64, 0, 64, 64));
+                gems.setColor(sf::Color(255, 255, 255, p.alpha));
+                gems.setPosition(p.x, p.y);
+                gems.move(offset.x - ts, offset.y - ts);
                 game.draw(gems);
                 gems.setScale(0.8f, 0.8f);
             }
